5ed969c2e4e5f61a15229521ef3a947f47a59a5d
LouisAugustine <nzlouis.com@gmail.com>
2025-10-06 16:28:57 +1300

    Update the comment icon

--- components/Home.tsx
+++ components/Home.tsx
@@ -123,7 +123,7 @@ export default function HomePage() {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth={2}
-                      d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                      d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                     />
                   </svg>
                   <span className="text-2xl font-bold text-gray-900">
